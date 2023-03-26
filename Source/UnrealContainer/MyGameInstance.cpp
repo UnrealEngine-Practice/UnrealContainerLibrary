1cb38a9218bba5ae100702e5cd5d38d7f27d27fe
ulol <jeffk0408@naver.com>
2023-03-26 18:28:13 +0900

    feat : TSet에서ì의 생성, 데이터 추가 , 데이터 삭제

--- Source/UnrealContainer/MyGameInstance.cpp
+++ Source/UnrealContainer/MyGameInstance.cpp
@@ -11,7 +11,7 @@ void UMyGameInstance::Init()
 	const int32 ArrayNum = 10;
 	TArray<int32> Int32Array;
 
-	for (int32 i = 0 ; i < ArrayNum ; ++i)
+	for (int32 i = 0 ; i <= ArrayNum ; ++i)
 	{
 		Int32Array.Add(i);
 	}
@@ -34,5 +34,20 @@ void UMyGameInstance::Init()
 
 	int32 Sum = Algo::Accumulate(Int32Array, 0);
 	UE_LOG(LogTemp, Log, TEXT("%d"), Sum);
-	
+
+	TSet<int32> Int32Set;
+	for (int32 i = 0 ; i <= ArrayNum; i++)
+	{
+		Int32Set.Add(i);
+	}
+	Int32Set.Remove(2);
+	Int32Set.Remove(4);
+	Int32Set.Remove(6);
+	Int32Set.Remove(8);
+	Int32Set.Remove(10);
+	Int32Set.Add(2);
+	Int32Set.Add(4);
+	Int32Set.Add(6);
+	Int32Set.Add(8);
+	Int32Set.Add(10);
 }
